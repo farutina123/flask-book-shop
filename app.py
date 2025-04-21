7bf956920dfcc268b92858150432cff71c93ee91
Полина <farutina97@gmail.com>
2025-04-21 15:07:36 +0300

    first

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from config import settings
+from db.database import init_db
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = settings.SECRET_KEY
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True, port=5466)
