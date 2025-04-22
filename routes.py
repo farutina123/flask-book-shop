fbb3e4aa2755d282bd6e8df22df671a46a1da577
Полина <farutina97@gmail.com>
2025-04-22 12:17:31 +0300

    open form

--- routes.py
+++ routes.py
@@ -1,14 +1,14 @@
 from flask import Blueprint, flash, redirect, url_for, render_template
+from flask_login import login_user, login_required, current_user, logout_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired, Length, Email, EqualTo
 from db.database import session_scope
 from db.models import User
-from werkzeug.security import generate_password_hash
-import email
+from werkzeug.security import generate_password_hash, check_password_hash
+import email_validator
 from click import confirm
-
-main_blueprint = Blueprint('main', '__name__')
+main_blueprint = Blueprint('main', __name__, url_prefix='/')
 
 
 class RegistrationForm(FlaskForm):
@@ -18,11 +18,16 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo("password")])
 
 
-@main_blueprint.route('main.register', methods=["GET", "POST"])
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[InputRequired(), Email()])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=36)])
+
+
+@main_blueprint.route('register', methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        with session_scope as session:
+        with session_scope() as session:
             user = session.query(User).filter_by(email=form.email.data).first()
         if user:
             flash('User with this email already exists', category='danger')
@@ -30,7 +35,7 @@ def register():
         user = User(username = form.username.data,
                     email = form.email.data,
                     password_hash = generate_password_hash(form.password.data))
-        with session_scope as session:
+        with session_scope() as session:
             session.add(user)
         return redirect(url_for('main.login'))
     elif form.errors:
@@ -38,6 +43,26 @@ def register():
     return render_template('register.html', form=form)
 
 
-@main_blueprint.route('/login')
+@main_blueprint.route('/main')
+@login_required
+def main_route():
+    return render_template('home.html', name = current_user.username)
+
+
+@main_blueprint.route('/login', methods=["GET", "POST"])
 def login():
-    pass
+    form = LoginForm()
+    if form.validate_on_submit():
+        with session_scope() as session:
+            user = session.query(User).filter_by(email=form.email.data).first()
+            if user and check_password_hash(user.password_hash, form.password.data):
+                login_user(user=user)
+                return redirect(url_for('main.main_route'))
+        flash('Login failed', 'danger')
+    return render_template('login.html', form=form)
+
+
+@main_blueprint.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.login'))
