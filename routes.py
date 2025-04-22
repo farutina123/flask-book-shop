a6951d8f739c96ac34dc05984d9a90da81b3c020
Полина <farutina97@gmail.com>
2025-04-22 22:11:44 +0300

    подтверждение по смс для авторизации

--- routes.py
+++ routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, redirect, url_for, render_template
+from flask import Blueprint, flash, redirect, url_for, render_template, request
 from flask_login import login_user, login_required, current_user, logout_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
@@ -6,6 +6,7 @@ from wtforms.validators import InputRequired, Length, Email, EqualTo
 from db.database import session_scope
 from db.models import User
 from werkzeug.security import generate_password_hash, check_password_hash
+import random
 import email_validator
 from click import confirm
 main_blueprint = Blueprint('main', __name__, url_prefix='/')
@@ -21,6 +22,27 @@ class RegistrationForm(FlaskForm):
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(), Email()])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=36)])
+
+
+class CodeForm(FlaskForm):
+    code = StringField('SMS', validators=[InputRequired(), Length(max=4, min=4)])
+
+
+@main_blueprint.route('/enter_code/<code>/<user_email>', methods=["GET", "POST"])
+def enter_code(code, user_email):
+    form = CodeForm()
+    if form.validate_on_submit():
+        with session_scope() as session:
+            user = session.query(User).filter_by(email=user_email).first()
+            if code == form.code.data:
+                login_user(user=user)
+                return redirect(url_for('main.main_route'))
+            else:
+                flash("код смс не верный", 'danger')
+                return redirect(url_for('main.login'))
+    elif form.errors:
+        flash(form.errors, category='danger')
+    return render_template('enter_code.html', form=form)
 
 
 @main_blueprint.route('register', methods=["GET", "POST"])
@@ -56,8 +78,9 @@ def login():
         with session_scope() as session:
             user = session.query(User).filter_by(email=form.email.data).first()
             if user and check_password_hash(user.password_hash, form.password.data):
-                login_user(user=user)
-                return redirect(url_for('main.main_route'))
+                code = ''.join(random.sample([str(x) for x in range(10)], 4))
+                form_code = CodeForm()
+                return render_template('enter_code.html', code=code, form=form_code, user_email=user.email)
         flash('Login failed', 'danger')
     return render_template('login.html', form=form)
 
